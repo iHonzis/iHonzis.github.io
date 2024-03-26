d0f6678b55eaee93a601790fd01135b2d0562dd0
Jan Hes <93439402+iHonzis@users.noreply.github.com>
2024-03-26 21:04:09 +0100

    funkční hledání

---
+++ iHonzis.github.io/nextpage/nextpage.js
@@ -0,0 +1,16 @@
+// Function to parse URL parameters
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+// Get the search string from the URL
+var searchString = getParameterByName('search');
+
+// Use the search string as needed
+console.log('Search string:', searchString);
